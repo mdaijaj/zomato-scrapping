729f3d86ee516342bb353398487d5203492a67a2
mdaijaj <41102052+mdaijaj@users.noreply.github.com>
2019-05-14 10:55:58 +0530

    Add files via upload

---
+++ zomato.py
@@ -0,0 +1,27 @@
+import requests,pprint
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+driver=webdriver.Chrome("/home/aijaj/Desktop/project/webscraping/chromedriver") #a
+driver.get("https://www.zomato.com/ncr")
+page=driver.execute_script("return document.documentElement.outerHTML")			#b
+driver.quit()
+
+def zomato_link():
+	soup=BeautifulSoup(page,"html.parser")
+	main_div=soup.find("div",class_="ui segment row")
+	place=main_div.find_all("a",class_="col-l-1by3 col-s-8 pbot0")
+	link_list=[]
+	counter=0
+	list_of_place=[]
+	for pl in place:
+		url=pl.get("href")
+		link_list.append(url)
+		all_place=(pl.text.strip().split())
+		counter+=1
+		places_list=(counter," ".join(all_place))
+		list_of_place.append(places_list)
+	# pprint.pprint(list_of_place)
+	return(link_list)		
+places=zomato_link()
+# pprint.pprint(places)
---
+++ zomato2.py
@@ -0,0 +1,57 @@
+import requests,pprint,json
+from zomato import zomato_link
+from bs4 import BeautifulSoup 
+from selenium import webdriver
+
+def resturant_details():
+	seemore_list=[]
+	for url in zomato_link():
+		driver=webdriver.Chrome("/home/aijaj/Desktop/project/webscraping/chromedriver")
+		driver.get(url)
+		page=driver.execute_script("return document.documentElement.outerHTML")
+		driver.quit()
+
+		soup=BeautifulSoup(page,"html.parser")
+		seemore=soup.find_all("a",class_="zred")
+		for j in seemore:
+			dic={}
+			link_see=j["href"]
+
+			driver2=webdriver.Chrome("/home/aijaj/Desktop/project/webscraping/chromedriver")
+			driver2.get(link_see)
+			page2=driver2.execute_script("return document.documentElement.outerHTML")
+			driver2.quit()
+
+			soup2=BeautifulSoup(page2,"html.parser")
+			main_div=soup2.find_all("div",class_="content")
+			for m in main_div:
+				rate=m.find("span",class_="col-s-11 col-m-12 pl0")
+				if (rate != None):
+					rate1=rate.text
+					dic["rate"]=rate1
+
+				rating=m.find("div",class_="ta-right floating search_result_rating col-s-4 clearfix")
+				if rating!=None:
+					rating1=rating.text.split()[0]
+					dic["rating"]=rating1
+
+				times=m.find("div",class_="col-s-11 col-m-12 pl0 search-grid-right-text ")
+				if (times!=None):
+					time=times.text.strip()	
+					dic["time"]=time
+
+				names=m.find("div",class_="col-s-12")
+				if names!=None:
+					data=names.text.strip().split("\n")
+					address=(data[-1])
+					if len(data)==5:
+						hotel_name=data[1]
+					else:
+						hotel_name=data[0]
+					dic["name"]=hotel_name
+					dic["address"]=address
+					pprint.pprint(dic)
+				
+			with open("zomato.json","w")as files:
+				inf=json.dump(dic,files,indent=4)
+resturant_details()
